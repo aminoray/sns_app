ca9ff401c9082a0725b93e5ee9f76909909b2528
EC2 Default User <ec2-user@ip-172-31-1-63.ap-northeast-1.compute.internal>
2023-06-26 00:42:44 +0000

    some fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  # PostsController
+  get 'posts/new', to: 'posts#new', as: 'new_post'
+  get '/', to: 'posts#index', as: 'index_post'
+
+  # TopicController
+  get 'topics/new', to: 'topics#new', as: 'new_topic'
+  get 'topics/edit', to: 'topics#edit', as: 'edit_topic'
 end
